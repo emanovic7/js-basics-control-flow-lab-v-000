4756dadccc9e3d3fd30538ea07c55385bed8249a
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 16:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Write your code in this file!
 
 
-function scuberGreetingForFeet(value){
-  const distance;
+function scuberGreetingForFeet(distance){
   if (distance =< 400){
     console.log("This one is on me!");
   }
