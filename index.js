bbae3fcffab95be8bb806e624d2c3ceef15132aa
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 16:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Write your code in this file!
 
 
-function scuberGreetingForFeet(distance){
-  if (distance <= 400){
-    return "This one is on me!";
-  }
-  else if (distance > 2000){
+function scuberGreetingForFeet (distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } 
+    else if (1999 < distance && distance < 2500) {
     return "I will gladly take your thirty bucks.";
-  }
-  else if (distance > 2500 ){
-    return "No can do";
+  } 
+    else {
+    return "No can do.";
   }
 }
